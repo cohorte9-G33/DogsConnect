d87342778ecd560b7b592c098af140a2f3ceb46f
cohorte9-G33 <125282729+cohorte9-G33@users.noreply.github.com>
2023-02-20 09:41:29 -0300

    Update index.js

--- back/index.js
+++ back/index.js
@@ -34,7 +34,7 @@ server.use(express.static(path.join(__dirname, 'public')));
 //Middleware
 server.use(express.urlencoded({ extended: false }));
 server.use(express.json());
-server.use(cors());
+server.use(cors({ origin:'*'}));
 
 //use routes
 server.use('/api/users', usersRoutes);
